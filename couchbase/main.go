0435930d5a1c8fd1433a52727fb7a073cb17aa8b
N N Advith <nnadvith08042001@gmail.com>
2025-03-30 15:15:01 +0530

    CRUD operations

--- couchbase/main.go
+++ couchbase/main.go
@@ -27,32 +27,27 @@ type CBConnector struct {
 func (c *CBConnector) Initialize(connectionString, bucketName, username, password string) error {
 
 	fmt.Println("Connecting to cluster and initialising bucket...")
-
 	c.ConnectionString = connectionString
 	c.BucketName = bucketName
 	c.Username = username
 	c.Password = password
-
 	cluster, err := gocb.Connect("couchbase://"+connectionString, gocb.ClusterOptions{
 		Authenticator: gocb.PasswordAuthenticator{
 			Username: c.Username,
 			Password: c.Password,
 		},
 	})
-
 	if err != nil {
 		return fmt.Errorf("authentication failed: %v", err)
 	}
 	c.Cluster = cluster
-
 	bucket := c.Cluster.Bucket(c.BucketName)
-
-	err = bucket.WaitUntilReady(3*time.Second, nil)
+	c.Bucket = bucket
+	err = c.Bucket.WaitUntilReady(3*time.Second, nil)
 	if err != nil {
 		return fmt.Errorf("bucket connection failed: %v", err)
 	}
 	fmt.Println("Cluster connected and bucket initialised")
-
 	return nil
 }
 
@@ -66,9 +61,83 @@ func (c *CBConnector) Disconnect() error {
 	return nil
 }
 
-// func addNote(note Note) {
+func (c *CBConnector) AddOrUpdateNote(key string, note Note, scope string, collection string) error {
+	//upsert
+	fmt.Println("adding documents")
+	col := c.Bucket.Scope(scope).Collection(collection)
+	_, err := col.Upsert(key, note, nil)
+	if err != nil {
+		return fmt.Errorf("error while upsert: %v", err)
+	}
+	fmt.Println("added documents")
+	return nil
+}
 
-// }
+func (c *CBConnector) GetNote(key string, scope string, collection string) error {
+	fmt.Println("retrieving note with ID ", key)
+	col := c.Bucket.Scope(scope).Collection(collection)
+	result, err := col.Get(key, nil)
+	if err != nil {
+		return fmt.Errorf("get operation failed : %v", err)
+	}
+	var note Note
+	if err := result.Content(&note); err != nil {
+		return fmt.Errorf("error decoding ; %v", err)
+	}
+	fmt.Println(note)
+	return nil
+}
+
+func (c *CBConnector) GetAllNotes(scope string, collection string) error {
+	fmt.Println("retrieving all notes")
+	sc := c.Bucket.Scope(scope)
+	results, err := sc.Query("SELECT * FROM "+string(collection), &gocb.QueryOptions{Adhoc: true})
+	if err != nil {
+		return fmt.Errorf("error performing query; %v", err)
+	}
+
+	for results.Next() {
+		var note map[string]Note
+		err := results.Row(&note)
+		if err != nil {
+			return fmt.Errorf("error decoding; %v", err)
+		}
+		fmt.Println(note["notes"])
+
+	}
+	return nil
+}
+
+func (c *CBConnector) GetNoteAndBG(scope string, collection string) error {
+	fmt.Println("retrieving note and bg color")
+	sc := c.Bucket.Scope(scope)
+	results, err := sc.Query("SELECT note,bgcolor FROM "+string(collection), &gocb.QueryOptions{Adhoc: true})
+	if err != nil {
+		return fmt.Errorf("error performing query; %v", err)
+	}
+
+	for results.Next() {
+		var note Note
+		err := results.Row(&note)
+		if err != nil {
+			return fmt.Errorf("error decoding; %v", err)
+		}
+		fmt.Println(note)
+
+	}
+	return nil
+}
+
+func (c *CBConnector) DeleteNote(key string, scope string, collection string) error {
+	fmt.Println("deleting note with ID ", key)
+	col := c.Bucket.Scope(scope).Collection(collection)
+	_, err := col.Remove(key, nil)
+	if err != nil {
+		return fmt.Errorf("remove operation failed : %v", err)
+	}
+	fmt.Println("removal completed")
+	return nil
+}
 
 func main() {
 	// gocb.SetLogger(gocb.DefaultStdioLogger())
@@ -91,76 +160,28 @@ func main() {
 		}
 	}()
 
-	// col := bucket.Scope("_default").Collection("users") //precreate this
-	// type User struct {
-	// 	Name      string   `json:"name"`
-	// 	Email     string   `json:"email"`
-	// 	Interests []string `json:"interests"`
-	// }
-	// _, err = col.Upsert("u:adam",
-	// 	User{
-	// 		Name:      "Adam",
-	// 		Email:     "adam@test-email.com",
-	// 		Interests: []string{"Swimming", "Rowing"},
-	// 	}, nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// getResult, err := col.Get("u:jade", nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// var inUser User
-	// err = getResult.Content(&inUser)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("User: %v\n", inUser)
-
-	// inventoryScope := bucket.Scope("_default")
-	// queryResult, err := inventoryScope.Query(
-	// 	"SELECT * FROM users",
-	// 	&gocb.QueryOptions{Adhoc: true},
-	// )
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// for queryResult.Next() {
-	// 	var result map[string]User
-	// 	// unkown json so decode into interfcae
-	// 	err := queryResult.Row(&result)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println(result["users"].Name)
-	// }
-
-	// if err := queryResult.Err(); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// queryResult, err = inventoryScope.Query(
-	// 	"SELECT name, email FROM users",
-	// 	&gocb.QueryOptions{Adhoc: true},
-	// )
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// for queryResult.Next() {
-	// 	var result User
-	// 	// unkown json so decode into interfcae
-	// 	err := queryResult.Row(&result)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println(result)
-	// }
-
-	// if err := queryResult.Err(); err != nil {
-	// 	log.Fatal(err)
-	// }
-
+	newnote := Note{
+		DateTime: string(time.DateTime),
+		Note:     "Get Good",
+		Origin:   "notes",
+		BGColor:  "#ff0000",
+	}
+	if err := connection.AddOrUpdateNote("n:note1", newnote, "_default", "notes"); err != nil {
+		fmt.Println(err)
+	}
+	if err := connection.AddOrUpdateNote("n:note2", newnote, "_default", "notes"); err != nil {
+		fmt.Println(err)
+	}
+	if err := connection.GetAllNotes("_default", "notes"); err != nil {
+		fmt.Println(err)
+	}
+	if err := connection.GetNoteAndBG("_default", "notes"); err != nil {
+		fmt.Println(err)
+	}
+	if err := connection.GetNote("n:note1", "_default", "notes"); err != nil {
+		fmt.Println(err)
+	}
+	if err := connection.DeleteNote("n:note1", "_default", "notes"); err != nil {
+		fmt.Println(err)
+	}
 }
